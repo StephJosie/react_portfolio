f5eaee623b8d797e4a098a991c85b531251442e9
steph <sjapodaca1103@gmail.com>
2022-03-13 19:06:37 -0700

    fixed readme

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../../Components/Header'
 import About from '../../Components/About'
-
+import Contact from '../../Components/Contact'
 
 export default function About() {
     return (
         <div>
             <Header />
             <About />
+            <Contact />
         </div>
     )
 }
